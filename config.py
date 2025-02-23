5e812e3878bbca630d0f9891d46a27c28ee2ab07
Natasha Ong <63530710+natashaongiscoding@users.noreply.github.com>
2025-02-24 11:42:58 +1300

    resolved merge conflicts

--- config.py
+++ config.py
@@ -1,6 +1,34 @@
-# config.py - TEMPORARY for demonstration only
-# WARNING: This is NOT safe for production! We'll fix it with Secrets Manager.
+import boto3
+from botocore.exceptions import ClientError
 
-AWS_ACCESS_KEY_ID = "AKIAW3MEFRAZ6EDHD2SR"
-AWS_SECRET_ACCESS_KEY = "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"
+def get_secret():
+
+    secret_name = "aws-access-key"
+    region_name = "us-east-2"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+
+# Retrieve credentials from Secrets Manager
+credentials = get_secret()
+
+# Extract the values; if AWS_REGION isn't in the secret, use the region from the session
+AWS_ACCESS_KEY_ID = credentials.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = credentials.get("AWS_SECRET_ACCESS_KEY")
 AWS_REGION = "us-east-2"
+
