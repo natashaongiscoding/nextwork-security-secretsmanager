f8adaa7d3e28235c5816f3c43ad1268479e2fec4
Natasha Ong <63530710+natashaongiscoding@users.noreply.github.com>
2025-03-01 14:59:25 +1300

    update config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-# config.py - TEMPORARY for demonstration only
-# WARNING: This is NOT safe for production! We'll fix it with Secrets Manager.
-
-AWS_ACCESS_KEY_ID = "YOUR_ACCESS_KEY_ID"
-AWS_SECRET_ACCESS_KEY = "YOUR_ACTUAL_ACCESS_KEY_ID"
-AWS_REGION = "us-east-2"
